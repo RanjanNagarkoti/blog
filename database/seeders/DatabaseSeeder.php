895e1aaf055219bee6cf8cdb9aafcb385a81318e
Ranjan Nagarkoti <ranjan.ngr2058@gmail.com>
2023-11-10 17:48:57 +0545

    feat: blog seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 use Database\Seeders\CategorySeeder;
+use Database\Seeders\BlogSeeder;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CategorySeeder::class,
+            BlogSeeder::class,
         ]);
     }
 }
