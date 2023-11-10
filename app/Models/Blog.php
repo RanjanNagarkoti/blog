a53b6d25d62b87f055d028173cd84092eebafb1f
Ranjan Nagarkoti <ranjan.ngr2058@gmail.com>
2023-11-10 15:36:43 +0545

    feat: eloquent realationship

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Blog extends Model
 {
@@ -14,4 +15,9 @@ class Blog extends Model
         'description',
         'category_id'
     ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
